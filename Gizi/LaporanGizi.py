ed9475dca9d0ce2f2031b6c1e6672967a6e3e5e1
aldimardiansah <aldimardiansah9@gmail.com>
2025-12-10 20:38:18 +0700

    feat: menambahkan fitur laporan gizi secara terpisah dari menu

---
+++ Gizi/LaporanGizi.py
@@ -0,0 +1,7 @@
+from .PencatatanGizi import lihat_catatan_user
+
+def laporan(username):
+    data = lihat_catatan_user(username)
+    print("\n--- LAPORAN GIZI ---")
+    for d in data:
+        print(f"{d['tanggal']} - {d['makanan']} | Kalori: {d['kalori']}")
