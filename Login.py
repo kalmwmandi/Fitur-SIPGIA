e05c766565c1d8e81592630625197b71f432a67b
aldimardiansah <aldimardiansah9@gmail.com>
2025-12-10 20:34:02 +0700

    fix: rename dan fixing login module

--- Login.py
+++ Login.py
@@ -1,23 +1,22 @@
-import PencatatanGiziHarian as pgh
+from Register import baca_database_user
 
 def loginAuth(username, password):
-    """Fungsi login user"""
-    users = pgh.baca_database_user()
+    users = baca_database_user()
     for user in users:
         if user["username"] == username and user["password"] == password:
             return user
     return None
 
+
 def login():
-    # Login
     print("\n--- LOGIN ---")
     username = input("Username: ")
     password = input("Password: ")
-    
+
     user = loginAuth(username, password)
     if user:
         print(f"\n[✓] Login berhasil! Selamat datang, {user['nama']}")
         return user
     else:
-        print("\n[X] Login gagal! Username atau password salah.")
+        print("\n[X] Username atau password salah.")
         return None
