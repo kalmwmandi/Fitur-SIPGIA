1789419fc7c11d75b499c181ed87cf019bdd6c3e
R4mFaaaw <rafamirzafirdaus@gmail.com>
2025-12-18 23:24:48 +0700

    Bagian Register ke-1

--- Register.py
+++ Register.py
@@ -1,4 +1,7 @@
 import os
+import re
+import stdiomask
+import string
 
 DB_USER = "database_user.txt"
 
@@ -24,19 +27,58 @@ def simpan_user(username, password, nama, role="user"):
     with open(DB_USER, "a") as f:
         f.write(f"{username}|{password}|{nama}|{role}\n")
 
+def is_only_characters(input_string):
+    pattern = r'^[a-zA-Z]+$'
+    return bool(re.match(pattern, input_string))
+
+print(f"'hello' only chars? {is_only_characters('hello')}") # True
+print(f"'hello123!' only chars? {is_only_characters('hello123!')}") # False
+
 
 def register():
     print("\n--- REGISTRASI ---")
-    username = input("Username baru: ")
-    password = input("Password: ")
-    nama = input("Nama lengkap: ")
+    username()
+    password()
+    nama()
+            
+def username():
+    while True:
+        username = input("Username: ")
+        if all(char.isalpha() or char.isspace() for char in username) and username.strip() != "":
+            print("Username")
+            users = baca_database_user()
+            for u in users:
+                if u["username"] == username:
+                    print("\n[X] Username sudah digunakan.")
+                return False
+            break
+        else:
+            print("Username tidak boleh ada simbol dan tidak boleh kosong!")
 
-    users = baca_database_user()
-    for u in users:
-        if u["username"] == username:
-            print("\n[X] Username sudah digunakan.")
-            return False
+def password():
+    while True:
+        password = stdiomask.getpass(prompt='Password: ', mask='*')
+        adaHurufBesar = any(char.isupper() for char in password)
+        adaSimbol = any(char in string.punctuation for char in password)
+        if len(password) >= 8 and bool(password) != adaHurufBesar and (password) != adaSimbol:
+            break
+        elif len(password) and password != adaHurufBesar and password != adaSimbol:
+            print("[X] Password tidak memenuhi syarat:\n- Minimal harus 8 karakter\n- Harus mengandung minimal 1 huruf besar\n- Harus mengandung minimal 1 simbol (contoh: !, @, #, $)")
+        elif password != adaHurufBesar and password != adaSimbol:
+            print("[X] Password tidak memenuhi syarat:\n- Harus mengandung minimal 1 huruf besar\n- Harus mengandung minimal 1 simbol (contoh: !, @, #, $)")
+        elif password != adaSimbol:
+            print("[X] Password tidak memenuhi syarat:\n- Harus mengandung minimal 1 simbol (contoh: !, @, #, $)")
+        else:
+            print("Password tidak boleh kosong")
 
-    simpan_user(username, password, nama)
-    print("\n[✓] Registrasi berhasil! Silakan login.")
-    return True
+def nama():
+    while True:
+        nama = input("Masukan nama: ")
+        if nama == "":
+            print("nama tidak boleh kosong!")
+        elif len(nama) < 30:
+            simpan_user(username, password, nama)
+            print("\n[✓] Registrasi berhasil! Silakan login.")
+            return True
+        break
+    
